435a66f6655ed43d93458c2231a4fe3fad46a406
bhagya <bhagyaraj@gmail.com>
2019-11-12 07:12:28 +0000

    first commit

---
+++ src/main/java/com/whey/App.java
@@ -0,0 +1,13 @@
+package com.whey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
